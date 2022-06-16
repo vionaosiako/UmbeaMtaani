9500e0e862585a4410848dcd297e2f7cb76e9b0b
vionaosiako <viona.osiako@student.morigaschool.com>
2022-06-17 00:15:10 +0300

    Connect to database and create static folder

--- UmbeaMtaaniProject/settings.py
+++ UmbeaMtaaniProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'NeighbourhoodApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'UmbeaMtaaniProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'umbeamtaani',
+        'USER': 'riziki',
+    'PASSWORD':'riziki',
     }
 }
 
